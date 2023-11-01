63c97a0b9ad41364b67fcebee6f0f85d355949d0
abdelhak45 <achtouiabdelhak4@gmail.com>
2023-11-01 14:58:58 +0100

    Ex:Portfolio commit

---
+++ main.js
@@ -0,0 +1,188 @@
+let ele1 = document.getElementById("ele1")
+let proImg1 = document.getElementById("proImg1")
+
+
+ele1.onmouseover= ()=>{
+    proImg1.style.cssText= "padding: 74px;margin-top: -123px;margin-bottom: -42px;transition: .5s;"
+}
+
+ele1.onmouseleave= ()=>{
+    proImg1.style.cssText= "padding: 20px;margin-top: none;margin-bottom: none;transition: .5s;"
+}
+
+// ==================================================================
+
+let ele2 = document.getElementById("ele2")
+let proImg2 = document.getElementById("proImg2")
+
+
+ele2.onmouseover= ()=>{
+    proImg2.style.cssText= "padding: 74px;margin-top: -123px;margin-bottom: -42px;transition: .5s;"
+}
+
+ele2.onmouseleave= ()=>{
+    proImg2.style.cssText= "padding: 20px;margin-top: none;margin-bottom: none;transition: .5s;"
+}
+
+// ==================================================================
+
+
+let ele3 = document.getElementById("ele3")
+let proImg3 = document.getElementById("proImg3")
+
+
+ele3.onmouseover= ()=>{
+    proImg3.style.cssText= "padding: 74px;margin-top: -123px;margin-bottom: -42px;transition: .5s;"
+}
+
+ele3.onmouseleave= ()=>{
+    proImg3.style.cssText= "padding: 20px;margin-top: none;margin-bottom: none;transition: .5s;"
+}
+
+// ==================================================================
+
+let ele4 = document.getElementById("ele4")
+let proImg4 = document.getElementById("proImg4")
+
+
+ele4.onmouseover= ()=>{
+    proImg4.style.cssText= "padding: 74px;margin-top: -123px;margin-bottom: -42px;transition: .5s;"
+}
+
+ele4.onmouseleave= ()=>{
+    proImg4.style.cssText= "padding: 20px;margin-top: none;margin-bottom: none;transition: .5s;"
+}
+
+// ==================================================================
+
+let ele5 = document.getElementById("ele5")
+let proImg5 = document.getElementById("proImg5")
+
+
+ele5.onmouseover= ()=>{
+    proImg5.style.cssText= "padding: 74px;margin-top: -123px;margin-bottom: -42px;transition: .5s;"
+}
+
+ele5.onmouseleave= ()=>{
+    proImg5.style.cssText= "padding: 20px;margin-top: none;margin-bottom: none;transition: .5s;"
+}
+// ==================================================================
+let ele6 = document.getElementById("ele6")
+let proImg6 = document.getElementById("proImg6")
+
+
+ele6.onmouseover= ()=>{
+    proImg6.style.cssText= "padding: 74px;margin-top: -123px;margin-bottom: -42px;transition: .5s;"
+}
+
+ele6.onmouseleave= ()=>{
+    proImg6.style.cssText= "padding: 20px;margin-top: none;margin-bottom: none;transition: .5s;"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// element.forEach(function(ele) {
+//     ele.onmouseover= function(){
+//         this.onmouseover= function(){
+//             proImg.document.getElementById("proImg").style.cssText= "padding: 74px;margin-top: -123px;margin-bottom: -42px;transition: .5s;"
+            
+//         }
+//         // console.log(this.document.getElementById("proImg"));
+//         // this.document.getElementById("proImg").style.cssText= "padding: 74px;margin-top: -123px;margin-bottom: -42px;transition: .5s;"
+//     }
+    
+//     // ele.onmouseleave= ()=>{
+
+//     //     this.document.getElementById("proImg").style.cssText= "padding: 20px;margin-top: none;margin-bottom: none;transition: .5s;"
+//     // }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // ele.forEach((e)=>{
+        //     e.onmouseleave= ()=>{
+        //         proImg.style.cssText= "padding: 20px;margin-top: none;margin-bottom: none;transition: .5s;"
+        //     }
+        // })
+
+// ele.onmouseover= ()=>{
+//     proImg.style.cssText= "padding: 74px;margin-top: -123px;margin-bottom: -42px;transition: .5s;"
+// }
+
+// ele.onmouseleave= ()=>{
+//     proImg.style.cssText= "padding: 20px;margin-top: none;margin-bottom: none;transition: .5s;"
+// }
+
+
+
+
+
+
